8989fae9776e8e88a9bfdc6de9fc61997ba45662
EVA-JianJun <eva_jianjun@163.com>
2021-09-07 11:52:50 +0800

    1.add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fd:
+    long_description = fd.read()
+
+setup(
+    name = 'Mconfig',
+    version = '1.0.0',
+    author = 'jianjun',
+    author_email = '910667956@qq.com',
+    url = 'https://github.com/EVA-JianJun/Mconfig',
+    description = u'.py 配置文件！Python最好用的配置文件！',
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    packages = find_packages(),
+    install_requires = [],
+    entry_points = {
+    }
+)
